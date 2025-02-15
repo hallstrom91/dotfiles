44225c8cdd4ec362b8602f289851d5c0431138d3
Simon Hällström <simonhallstrom@live.se>
2025-02-15 15:19:07 +0100

    update shell-configs script

--- setup-shell-configs.sh
+++ setup-shell-configs.sh
@@ -6,6 +6,7 @@ DOTFILES_DIR=~/Documents/dotfiles
 BASH_DIR="$DOTFILES_DIR/bash"
 SHELL_DIR="$DOTFILES_DIR/shell"
 CUSTOM_SCRIPTS="$SHELL_DIR/custom-scripts"
+HOSTNAME=$(hostname)
 
 HOME_BIN=~/.bin
 GPG_DIR=~/.gnupg
@@ -28,10 +29,28 @@ echo "Copying GPG agent configuration to ~/.gnupg/"
 mkdir -p "$GPG_DIR"
 cp -v "$SHELL_DIR/gpg-agent.conf" "$GPG_DIR/"
 
-echo "Moving custom scripts to ~/.bin/ and making them executable..."
-for script in "$CUSTOM_SCRIPTS"/*.sh; do
-  ln -sf "$script" "$HOME_BIN/$(basename "$script")"
-  chmod +x "$HOME_BIN/$(basename "$script")"
-done
+if [[ "$HOSTNAME" == "pop-os" ]]; then
+  echo "Detected Desktop (pop-os) - Using default scripts"
+  ln -sf "$CUSTOM_SCRIPTS/mountwsx.sh" "$HOME_BIN/mountwsx.sh"
+  ln -sf "$CUSTOM_SCRIPTS/mountwsx_and_navigate.sh" "$HOME_BIN/mountwsx_and_navigate.sh"
+  ln -sf "$CUSTOM_SCRIPTS/dismountwsx.sh" "$HOME_BIN/dismountwsx.sh"
+elif [[ "$HOSTNAME" == "laptop" ]]; then
+  echo "Detected Laptop (laptop): Using modified scripts"
+  ln -sf "$CUSTOM_SCRIPTS/laptop/mountwsx_laptop.sh" "$HOME_BIN/mountwsx.sh"
+  ln -sf "$CUSTOM_SCRIPTS/laptop/mountwsx_and_nav_laptop.sh" "$HOME_BIN/laptop/mountwsx_and_navigate.sh"
+  ln -sf "$CUSTOM_SCRIPTS/laptop/dismountwsx_laptop.sh" "$HOME_BIN/laptop/dismountwsx.sh"
+else
+  echo "Unkown host ($HOSTNAME), self destruct..."
+fi
+
+ln -sf "$CUSTOM_SCRIPTS/untar.sh" "$HOME_BIN/untar.sh"
+
+chmod +x "$HOME_BIN"/*.sh
+
+# echo "Moving custom scripts to ~/.bin/ and making them executable..."
+# for script in "$CUSTOM_SCRIPTS"/*.sh; do
+#   ln -sf "$script" "$HOME_BIN/$(basename "$script")"
+#   chmod +x "$HOME_BIN/$(basename "$script")"
+# done
 
 echo "Shell and bash configurations successfully set up!"
