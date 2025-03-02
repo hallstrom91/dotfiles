90c6beeeae7ae79240f339c20e45920771dbc876
Simon Hällström <simonhallstrom@live.se>
2025-03-02 16:46:17 +0100

    core settings updated

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -1,34 +1,43 @@
 local autocmd = vim.api.nvim_create_autocmd
 
--- Format on save
+local augroup = function(name)
+  return vim.api.nvim_create_augroup(name, { clear = true })
+end
+
+--> Format on save
 autocmd('BufWritePre', {
+  group = augroup('AutoFormat'),
   pattern = { '*', '*.md' },
   callback = function(args)
     require('conform').format({ bufnr = args.buf })
   end,
 })
 
--- open help window in vertical mode
+--> open help window in vertical mode
 autocmd('FileType', {
+  group = augroup('HelpWindow'),
   pattern = 'help',
   command = 'wincmd L',
 })
 
 -- Auto update Treesitter Parsers
 autocmd('VimEnter', {
+  group = augroup('TreesitterUpdate'),
   pattern = 'VeryLazy',
   callback = function()
     vim.cmd('TSUpdate')
   end,
 })
 
--- Recognize .bash files
+-- Recognize .bash_* files
 autocmd({ 'BufRead', 'BufNewFile' }, {
+  group = augroup('BashFiles'),
   pattern = '.bash_*',
   command = 'set filetype=bash',
 })
 
 autocmd('CursorMoved', {
+  group = augroup('CursorHighlight'),
   callback = function()
     local buftype = vim.api.nvim_get_option_value('buftype', { buf = 0 })
     if buftype == '' then
@@ -38,5 +47,47 @@ autocmd('CursorMoved', {
         vim.cmd('setlocal nocursorline')
       end, 300)
     end
+  end,
+})
+
+--> Highlight on yank
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = augroup('HighlightYank'),
+  callback = function()
+    (vim.hl or vim.highlight).on_yank()
+  end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup('ClosePlugin'),
+  pattern = {
+    'PlenaryTestPopup',
+    'checkhealth',
+    'dbout',
+    'gitsigns-blame',
+    'grug-far',
+    'help',
+    'lspinfo',
+    'neotest-output',
+    'neotest-output-panel',
+    'neotest-summary',
+    'notify',
+    'qf',
+    'spectre_panel',
+    'startuptime',
+    'tsplayground',
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.schedule(function()
+      vim.keymap.set('n', 'q', function()
+        vim.cmd('close')
+        pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+      end, {
+        buffer = event.buf,
+        silent = true,
+        desc = 'Quit buffer',
+      })
+    end)
   end,
 })
--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -1,27 +1,32 @@
+local opt = vim.opt
 -- Load my default settings for nvim
-vim.o.number = true -- Show line numbers
-vim.o.relativenumber = false -- Relative line numbers (very annoying)
-vim.o.expandtab = true -- Use spaces instead of tabs
-vim.o.shiftwidth = 4 -- Indentation width
-vim.o.tabstop = 4 -- Tab width
-vim.o.termguicolors = true -- Enable true colors
-vim.o.splitbelow = false -- Open windows horizontal
-vim.o.splitright = true -- Open windows vertical
+opt.number = true -- Show line numbers
+opt.relativenumber = false -- Relative line numbers (very annoying)
+opt.expandtab = true -- Use spaces instead of tabs
+opt.shiftwidth = 4 -- Indentation width
+opt.tabstop = 4 -- Tab width
+opt.termguicolors = true -- Enable true colors
+opt.splitbelow = false -- Open windows horizontal
+opt.splitright = true -- Open windows vertical
+opt.foldlevel = 99
+opt.foldlevelstart = 99
+opt.foldcolumn = '1'
+opt.foldenable = true
+opt.foldexpr = 'v:lua.require("ufo").foldexpr()'
+opt.foldmethod = 'expr'
+opt.linebreak = true
+opt.wrap = false -- disable line wrap
+opt.smoothscroll = true
+opt.clipboard = vim.env.SSH_TTY and '' or 'unnamedplus'
+--opt.clipboard = 'unnamedplus' -- sync clipboard actions with system clipboard
+--opt.foldexpr = "nvim_treesitter#foldexpr()"
+-- opt.mousemoveevent = true -- "true" to activate hoover on buffertabs
+
 vim.g.loaded_netrw = 1 -- disable netrw
 vim.g.loaded_netrwPlugin = 1 -- disable netrw
-vim.o.foldlevel = 99
-vim.o.foldlevelstart = 99
-vim.o.foldcolumn = '1'
-vim.o.foldenable = true
-vim.o.foldexpr = 'v:lua.require("ufo").foldexpr()'
-vim.o.foldmethod = 'expr'
--- vim.o.mousemoveevent = true -- "true" to activate hoover on buffertabs
---vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+vim.g.markdown_recommended_style = 0
 
-vim.opt.clipboard = 'unnamedplus' -- sync clipboard actions with system clipboard
---vim.opt.guicursor = "n-v-c-i:ver25" -- changes all cursor markers to thin |
-
-vim.opt.guicursor = {
+opt.guicursor = {
   -- Normal mode: horizontal line _
   'n-v-c:hor25',
   -- Insert mode: vertical thin line |
@@ -29,6 +34,15 @@ vim.opt.guicursor = {
   -- Command line - vertical
   'c:ver25',
 }
+
+-- opt.fillchars = {
+--   foldopen = "",
+--   foldclose = "",
+--   fold = " ",
+--   foldsep = " ",
+--   diff = "╱",
+--   eob = " ",
+-- }
 
 -- Disable providers
 vim.g.loaded_node_provider = 0
